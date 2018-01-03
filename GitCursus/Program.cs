8d682a1d8e6804e994998622a8c06b030ebf9d99
placetobejohan <johan.md.maes@gmail.com>
2018-01-03 18:43:59 +0100

    iedereen

--- GitCursus/Program.cs
+++ GitCursus/Program.cs
@@ -10,7 +10,7 @@ namespace GitCursus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bonjour tout le monde");
+            Console.WriteLine("Hallo iedereen");
         }
     }
 }
