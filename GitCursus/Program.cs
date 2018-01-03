72a6e07cf42b9a91bee459f151c225873f0ef976
Johan <johan.md.maes@gmail.com>
2018-01-03 19:01:50 +0100

    houden van

--- GitCursus/Program.cs
+++ GitCursus/Program.cs
@@ -10,7 +10,7 @@ namespace GitCursus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo allemaal");
+            Console.WriteLine("Ik hou van u");
         }
     }
 }
